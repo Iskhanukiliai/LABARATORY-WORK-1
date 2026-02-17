7fbfc0bafba847cc6f4446fce49000731a48ebee
Iskhan Ukiliai <ukiliai.iskhan@narxoz.kz>
2026-02-17 09:07:57 +0500

    3 задача

--- lab 1/LABARATORY WORK 1.py
+++ lab 1/LABARATORY WORK 1.py
@@ -1,30 +1,26 @@
-# задача 2
-matyn = input("мәтін енгіз:")
-def main(text):
-    result =[]
-    for word in text.split():
-        digit = False
-        for ch in word:
-            if ch.isdigit():
-                digit = True
-                break
-        if not digit:
-            word2 = word[::-1]
-            if len(word2) % 2 == 0:
-                result.append(word2)
-    return " ".join(result)
-print(main(matyn))
-
-# lambda
-main = lambda text:" ".join(
-    map(
-        lambda word: word[::-1],
-        filter(
-            lambda word: not any(ch.isdigit() for ch in word) and len(word) % 2 == 0,
-        text.split()
-        )
-    )
-)
-matyn = input("matyn engiz:")
-print(main(matyn))
-
+# 3 задача
+def top_k_words(text, k):
+    text = text.lower()
+    clean = ""
+    for c in text:
+        if c.isalnum() or c == " ":
+            clean += c
+    words = clean.split()
+    uniq = []
+    cnt = []
+    for w in words:
+        if w in uniq:
+            i = uniq.index(w)
+            cnt[i] += 1
+        else:
+            uniq.append(w)
+            cnt.append(1)
+    for i in range(len(uniq)):
+        for j in range(len(uniq)-1):
+            if cnt[j] < cnt[j+1] or (cnt[j]==cnt[j+1] and uniq[j] > uniq[j+1]):
+                cnt[j], cnt[j+1] = cnt[j+1], cnt[j]
+                uniq[j], uniq[j+1] = uniq[j+1], uniq[j]
+    res = []
+    for i in range(min(k, len(uniq))):
+        res.append(uniq[i])
+    return res
