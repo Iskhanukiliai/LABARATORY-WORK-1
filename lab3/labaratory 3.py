6db7385d9bb1a64e31d4d53559b561f1459a4067
Iskhan Ukiliai <ukiliai.iskhan@narxoz.kz>
2026-03-07 15:30:59 +0500

    ex 3

--- lab3/labaratory 3.py
+++ lab3/labaratory 3.py
@@ -8,5 +8,9 @@ words = ["арбуз", "кот", "машина", "дом", "ананас"]
 word = sorted(words, key = lambda x: (len(x) , words[0]))
 print(word)
 
+#ex 3
+numbers = [5, 12, 7, 20, 33, 8]
+num = list(filter(lambda i : i % 2 == 0 and i > 10, numbers))
+print(num)
 
 
