54564f4357645bb310fc0eb20bc8fc04f8d13c6d
Iskhan Ukiliai <ukiliai.iskhan@narxoz.kz>
2026-03-18 23:34:45 +0500

    ex 18

--- lab3/labaratory 3.py
+++ lab3/labaratory 3.py
@@ -150,4 +150,14 @@ print(result)
 print("==========================")
 
 
+# ex 18
+words = ["кот", "машина", "арбуз", "дом", "ананас"]
+result = [
+    (word.upper() if len(word) > 4 else "short") + ("*" if "а" in word else "")
+    for word in words
+]
+print(result)
+print("==========================")
+
+
 
