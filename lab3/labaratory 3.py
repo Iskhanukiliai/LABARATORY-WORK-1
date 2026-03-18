0be1896a1b70ca24b503b30a48f88af8b679850b
Iskhan Ukiliai <ukiliai.iskhan@narxoz.kz>
2026-03-19 00:12:57 +0500

    ex 19

--- lab3/labaratory 3.py
+++ lab3/labaratory 3.py
@@ -150,7 +150,7 @@ print(result)
 print("==========================")
 
 
-# ex 18
+# ex 19
 words = ["кот", "машина", "арбуз", "дом", "ананас"]
 result = [
     (word.upper() if len(word) > 4 else "short") + ("*" if "а" in word else "")
@@ -158,6 +158,33 @@ result = [
 ]
 print(result)
 print("==========================")
+
+# ex 18
+def prime(n):
+    if n < 2:
+        return False
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
+
+def special_numbers(n):
+    for x in range(1, n + 1):
+        if x % 3 == 0 and x % 5 == 0:
+            yield "FizzBuzz"
+        elif x % 3 == 0:
+            yield "Fizz"
+        elif x % 5 == 0:
+            yield "Buzz"
+        elif prime(x):
+            yield "простое"
+        else:
+            yield x
+for n in special_numbers(15):
+    print(n)
+
+
+
 
 
 
--- проверка.py
+++ проверка.py
@@ -1,8 +1,22 @@
-words = ["кот", "машина", "арбуз", "дом", "ананас"]
+def prime(n):
+    if n < 2:
+        return False
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
 
-result = [
-    (word.upper() if len(word) > 4 else "short") + ("*" if "а" in word else "")
-    for word in words
-]
-
-print(result)
+def special_numbers(n):
+    for x in range(1, n + 1):
+        if x % 3 == 0 and x % 5 == 0:
+            yield "FizzBuzz"
+        elif x % 3 == 0:
+            yield "Fizz"
+        elif x % 5 == 0:
+            yield "Buzz"
+        elif prime(x):
+            yield "простое"
+        else:
+            yield x
+for n in special_numbers(15):
+    print(n)
