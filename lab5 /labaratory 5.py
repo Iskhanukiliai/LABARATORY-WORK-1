2577175705295160382bf434851a55b451a4b177
Iskhan Ukiliai <ukiliai.iskhan@narxoz.kz>
2026-04-16 16:34:57 +0500

    task 2

--- lab5 /labaratory 5.py
+++ lab5 /labaratory 5.py
@@ -1,3 +1,4 @@
+#task 1
 class User:
     def __init__(self, user_id, name, email):
 
@@ -14,3 +15,12 @@ class User:
 
     def __del__(self):
         print(f"User {self._name} deleted")
+#task 2
+@classmethod
+    def from_string(cls, data: str):
+        user_id_str, name, email = data.split(',')
+        return cls(int(user_id_str.strip()), name.strip(), email.strip())
+u1 = User(1, "john doe", "John@Example.COM")
+print(u1)
+u2 = User.from_string("2, Alice Wonderland, alice@wonder.com")
+print(u2)
