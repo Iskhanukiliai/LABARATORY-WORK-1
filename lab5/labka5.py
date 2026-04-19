5006de082d04c01dfb4aae820d1d19efc3ef07b5
Iskhan Ukiliai <ukiliai.iskhan@narxoz.kz>
2026-04-19 22:32:22 +0500

    task2_new

--- lab5/labka5.py
+++ lab5/labka5.py
@@ -17,6 +17,41 @@ def test_user():
     u = User(1, "  john doe  ", "John@Example.COM")
     return {"data": u.to_dict(), "str_repr": str(u)}
 
+
+
+#2 esep
+    @classmethod
+    def from_string(cls, data: str):
+        try:
+            parts = data.split(',')
+            if len(parts) != 3:
+                raise ValueError("Жолда үтір арқылы 3 элемент болуы керек: id, name, email")
+            user_id = int(parts[0].strip())
+            name = parts[1].strip()
+            email = parts[2].strip()
+            return cls(user_id, name, email)
+        except (ValueError, IndexError):
+            raise ValueError("Жолдағы деректердің дұрыс емес форматы")
+    def __str__(self):
+        return f"User(id={self._id}, name='{self._name}', email='{self._email}')"
+    def to_dict(self):
+        return {"id": self._id, "name": self._name, "email": self._email}
+
+@app.get("/user/from-string")
+def test_from_string():
+    try:
+        raw_data = "2, Alice Wonderland , alice@wonder.com"
+        u = User.from_string(raw_data)
+        return {
+            "source_string": raw_data,
+            "created_object": u.to_dict(),
+            "str_repr": str(u) }
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+
+
 @app.get("/home")
 def home():
-    return "Добро пожаловать в магазин!"
+    return "Дүкенге қош келдіңіз!"
+
